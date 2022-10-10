598921dd22710b4c1ec08e35d4d2c4b87385025f
Alex Stark <alexcstark@gmail.com>
2022-10-09 17:03:12 -0700

    export tws as api

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
+val artifactRegistryMavenSecret: String by project
 plugins {
-    `java`
+    java
     id("java-library")
     id("idea")
-    id("io.freefair.lombok") version "6.5.0.2"
+    id("io.freefair.lombok") version "6.5.0.2" //    id("com.google.cloud.artifactregistry.gradle-plugin") version "2.1.5"
     `maven-publish`
 }
 
@@ -14,8 +15,8 @@ repositories {
 }
 
 dependencies {
-    implementation(files("lib/TwsApi.jar"))
-    implementation("io.projectreactor:reactor-core:3.4.18")
+    api(files("lib/TwsApi.jar"))
+    api("io.projectreactor:reactor-core:3.4.18")
     implementation("javax.annotation:javax.annotation-api:1.3.2")
     implementation("com.google.guava:guava:31.1-jre")
     implementation("joda-time:joda-time:2.10.14")
@@ -30,8 +31,8 @@ allprojects {
 }
 
 java {
-    sourceCompatibility = org.gradle.api.JavaVersion.VERSION_18
-    targetCompatibility = org.gradle.api.JavaVersion.VERSION_18
+    sourceCompatibility = JavaVersion.VERSION_18
+    targetCompatibility = JavaVersion.VERSION_18
 }
 
 tasks.getByName<Test>("test") {
@@ -39,14 +40,51 @@ tasks.getByName<Test>("test") {
 }
 
 
+//publishing {
+//}
 publishing {
     publications {
         create<MavenPublication>("maven") {
             groupId = "com.github.wisp-capital"
             artifactId = "ib_reactor"
-            version = "1.0.2"
+            version = "1.0.5"
 
             from(components["java"])
         }
     }
+    repositories {
+        maven {
+            url = uri("https://us-west1-maven.pkg.dev/iocore/wisp-capital-jvm")
+            credentials {
+                username = "_json_key_base64"
+                password = artifactRegistryMavenSecret
+            }
+
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+        }
+    }
 }
+
+repositories {
+    maven {
+        url = uri("https://us-west1-maven.pkg.dev/iocore/wisp-capital-jvm")
+        credentials {
+            username = "_json_key_base64"
+            password = artifactRegistryMavenSecret
+        }
+
+        authentication {
+            create<BasicAuthentication>("basic")
+        }
+    }
+}
+
+tasks.register("printProps") {
+    doLast {
+        println(artifactRegistryMavenSecret)
+    }
+}
+
+
